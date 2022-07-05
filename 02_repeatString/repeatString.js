d55aee7fe4aa601fafa844f6ca13304cf36ad116
DanTheKim <danthekim98@gmail.com>
2022-07-05 14:21:19 -0400

    Completed Tests 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, iter) {
+    let result = '';
 
+    if(iter >= 0){
+        for(let i = 0; i < iter; i++){
+            result = result + word;
+        }
+
+        return result;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
