584253a341a499ddccd8ec7f4983844ae142ae99
DanTheKim <danthekim98@gmail.com>
2022-07-09 23:08:40 -0400

    Completed 04_removeFromArray problem

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+    for(let j = 0; j < theArgs.length; j++){
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i] === theArgs[j]){
+                arr.splice(i, 1);
+            }
+        }
+    }
 
+    return arr;
 };
 
 // Do not edit below this line
