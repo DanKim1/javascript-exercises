ba35acac6c794ce69bd67b2ce347955a0f4b640e
DanTheKim <danthekim98@gmail.com>
2022-07-09 21:55:20 -0400

    Fixed 03_reverseString problem

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(revString) {
+    let resultString = '';
 
+    for(let i = revString.length; i > 0; i--){
+        resultString = resultString + revString[i -1];
+    }    
+
+    return resultString;
 };
 
 // Do not edit below this line
