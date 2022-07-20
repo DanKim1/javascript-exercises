ba8bd634b0884dc6ccf5f9b11d3c4107f7def152
DanTheKim <danthekim98@gmail.com>
2022-07-20 00:57:38 -0400

    Completed -5_sumAll Problem

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let total = 0;
 
+    if(startNum > endNum){
+        for(let i = startNum; i >= endNum; i--){
+            total = total + i;
+        }
+    }
+    else{
+        for(let j = startNum; j <= endNum; j++){
+            total = total + j
+        }
+    }
+
+    if(startNum < 0 || endNum < 0 || !Number.isInteger(startNum) || !Number.isInteger(endNum)){
+        total = 'ERROR';
+    }
+        return total;
 };
 
 // Do not edit below this line
